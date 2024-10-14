93cc5826da5e88ae936fee527464b7ffb19118d1
Tom Lin <tom91136@gmail.com>
2024-10-14 05:46:15 +0100

    Expose all.hpp header

---
+++ src/aspartame/all.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "aspartame/deque.hpp"
+#include "aspartame/list.hpp"
+#include "aspartame/map.hpp"
+#include "aspartame/optional.hpp"
+#include "aspartame/set.hpp"
+#include "aspartame/string.hpp"
+#include "aspartame/unordered_map.hpp"
+#include "aspartame/unordered_set.hpp"
+#include "aspartame/variant.hpp"
+#include "aspartame/vector.hpp"
+#include "aspartame/view.hpp"
--- test/test_base_includes.hpp
+++ test/test_base_includes.hpp
@@ -1,13 +1,3 @@
 #pragma once
 // XXX include everything to test correct overload resolution
-#include "aspartame/deque.hpp"
-#include "aspartame/list.hpp"
-#include "aspartame/map.hpp"
-#include "aspartame/optional.hpp"
-#include "aspartame/set.hpp"
-#include "aspartame/string.hpp"
-#include "aspartame/unordered_map.hpp"
-#include "aspartame/unordered_set.hpp"
-#include "aspartame/vector.hpp"
-#include "aspartame/view.hpp"
-#include "aspartame/variant.hpp"
+#include "aspartame/all.hpp"
