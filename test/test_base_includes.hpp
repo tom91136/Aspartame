68d83588eaa529cb7c74319c492caaaeea59cf20
Tom Lin <tom91136@gmail.com>
2026-06-01 02:22:29 +0100

    Fix CI 3

--- test/test_base_includes.hpp
+++ test/test_base_includes.hpp
@@ -3,3 +3,13 @@
 #define ASPARTAME_SHOW_STREAM
 #endif
 #include "aspartame/all.hpp"
+
+// XXX Workaround for Catch2 + clang + libstdc++ 14 + c++23: optional's <=> rewrite
+// probes std::tuple_size<Catch::Decomposer> with a hard error instead of SFINAE.
+// See https://github.com/catchorg/Catch2/issues/2923 and llvm/llvm-project#113087
+#include <cstddef>
+#include <tuple>
+namespace Catch { struct Decomposer; }
+namespace std {
+template <> struct tuple_size<Catch::Decomposer> : integral_constant<size_t, 1> {};
+} // namespace std
